a03edf51157c71f36eabdab1c908bbad37af15c3
Hanzio <d_mbm@hotmail.com>
2023-06-14 16:11:54 -0500

    elimino el estilo que forzaba un height 100% en el banner

--- app/public/wp-content/themes/peruconexiontravel/galeria.php
+++ app/public/wp-content/themes/peruconexiontravel/galeria.php
@@ -1,20 +1,20 @@
 <div class="Gallery-Image">
-<?php $images = get_field('galeria');
-if( $images ) { ?>
+    <?php $images = get_field('galeria');
+    if ($images) { ?>
         <div id="Gal" class="Galeria-img owl-carousel owl-theme">
-           <?php foreach( $images as $image ): ?>
-            <div class="Item-Galeria">
-                  <img src="<?php echo $image['sizes']['galeria']; ?>" alt="<?php echo $image['alt']; ?>" />
-            </div>
+            <?php foreach ($images as $image) : ?>
+                <div class="Item-Galeria">
+                    <img src="<?php echo $image['sizes']['galeria']; ?>" alt="<?php echo $image['alt']; ?>" />
+                </div>
             <?php endforeach; ?>
         </div>
-<?php } else { ?>
-<div class="galeriaSingle reset">
-<?php { ?>
-<div class="Item-Galeria">
-    <img src="<?php bloginfo('template_directory'); ?>/images/BG-galeria.jpg" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
-</div>
-<?php } ?>
-</div>             
-<?php } ?>
-</div>
+    <?php } else { ?>
+        <div class="galeriaSingle reset">
+            <?php { ?>
+                <div class="Item-Galeria">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/BG-galeria.jpg" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                </div>
+            <?php } ?>
+        </div>
+    <?php } ?>
+</div>∫∫
